285fd1e35c196b2438716be712fb5fb1f718a784
Raymon Mens <r.mens@me.com>
2022-02-12 22:58:42 +0100

    Include rebroadcast in stream.sh

--- stream.sh
+++ stream.sh
@@ -4,6 +4,7 @@ docker rm livestream
 
 # Get and save nginx configuration
 wget https://raw.githubusercontent.com/oszuidwest/nginx-rtmp-live/main/nginx.conf -O /root/nginx.conf.template
+wget https://raw.githubusercontent.com/oszuidwest/nginx-rtmp-live/main/rebroadcast.conf -O /root/rebroadcast.conf
 
 # Start new container
 docker run -it \
@@ -11,6 +12,7 @@ docker run -it \
   --name livestream \
   -p 1935:1935 -p 80:80 -p 443:443 \
   --mount type=bind,source="/root/nginx.conf.template",target="/etc/nginx/nginx.conf.template" \
+  --mount type=bind,source="/root/rebroadcast.conf",target="/etc/nginx/rebroadcast.conf" \
   --mount type=bind,source="/etc/letsencrypt/live/live.zuidwesttv.nl/privkey.pem",target="/opt/certs/live.zuidwesttv.nl.key" \
   --mount type=bind,source="/etc/letsencrypt/live/live.zuidwesttv.nl/fullchain.pem",target="/opt/certs/live.zuidwesttv.nl.crt" \
   alfg/nginx-rtmp
