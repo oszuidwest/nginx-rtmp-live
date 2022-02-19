81bce132605eb5a1db66b1cbf29d78daebe9c69d
rmens <r.mens@me.com>
2022-02-19 14:02:26 +0100

    Streamline template name for consistency

--- stream.sh
+++ stream.sh
@@ -3,7 +3,7 @@ docker stop livestream
 docker rm livestream
 
 # Get and save nginx configuration
-wget https://raw.githubusercontent.com/oszuidwest/nginx-rtmp-live/main/nginx.conf -O /root/nginx.conf.template
+wget https://raw.githubusercontent.com/oszuidwest/nginx-rtmp-live/main/nginx.conf -O /root/nginx.conf
 wget https://raw.githubusercontent.com/oszuidwest/nginx-rtmp-live/main/rebroadcast.conf -O /root/rebroadcast.conf
 
 # Start new container
@@ -11,7 +11,7 @@ docker run -it \
   --restart unless-stopped \
   --name livestream \
   -p 1935:1935 -p 80:80 -p 443:443 \
-  --mount type=bind,source="/root/nginx.conf.template",target="/etc/nginx/nginx.conf.template" \
+  --mount type=bind,source="/root/nginx.conf",target="/etc/nginx/nginx.conf.template" \
   --mount type=bind,source="/root/rebroadcast.conf",target="/etc/nginx/rebroadcast.conf" \
   --mount type=bind,source="/etc/letsencrypt/live/live.zuidwesttv.nl/privkey.pem",target="/opt/certs/live.zuidwesttv.nl.key" \
   --mount type=bind,source="/etc/letsencrypt/live/live.zuidwesttv.nl/fullchain.pem",target="/opt/certs/live.zuidwesttv.nl.crt" \
